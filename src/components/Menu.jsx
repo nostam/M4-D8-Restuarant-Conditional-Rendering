7e67d03ec312bd5223096584ec69cbc6fe0c2c5a
nostam <matsonho@gmail.com>
2020-11-25 15:41:00 +0100

    add data handling in Menu component

--- src/components/Menu.jsx
+++ src/components/Menu.jsx
@@ -1,11 +1,15 @@
 import React from "react";
 import dishes from "../data/menu.json";
 import SingleDish from "./SingleDish.jsx";
-import { Container } from "react-bootstrap";
-
+import { Container, Alert } from "react-bootstrap";
+// const dishes = [];
 const Menu = () => (
   <Container>
-    {dishes && dishes.map((dish) => <SingleDish dish={dish} key={dish.id} />)}
+    {dishes.length === 0 && (
+      <Alert variant="info">Working in Progress to get all the 🍝</Alert>
+    )}
+    {dishes.length !== 0 &&
+      dishes.map((dish) => <SingleDish dish={dish} key={dish.id} />)}
   </Container>
 );
 export default Menu;
