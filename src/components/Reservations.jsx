7bc31389b6785b1bd7c117f752a4ebd650f7ed3f
nostam <matsonho@gmail.com>
2020-11-25 14:43:40 +0100

    add error handling in Reservation component

--- src/components/Reservations.jsx
+++ src/components/Reservations.jsx
@@ -1,58 +1,65 @@
-import React from 'react'
-import { Container, ListGroup, Spinner } from 'react-bootstrap'
-import ReservationForm from './ReservationForm'
+import React from "react";
+import { Container, ListGroup, Spinner, Alert } from "react-bootstrap";
+import ReservationForm from "./ReservationForm";
 
 class Reservations extends React.Component {
+  state = {
+    reservations: [],
+    loading: true,
+    errMessage: "",
+  };
 
-    state = {
-        reservations: [],
-        loading: true
+  // componentWillUnmount fires an instant before unmounting
+
+  //lifecycle method that is going to be triggered just once after initial loading
+  componentDidMount = async () => {
+    try {
+      let response = await fetch(
+        "https://striveschool.herokuapp.com/api/reservation"
+      );
+      let reservations = await response.json();
+      this.setState({ reservations: reservations, loading: false });
+    } catch (e) {
+      console.log("error happened, that's life", e);
+      this.setState({ loading: false, errMessage: e.message });
     }
+  };
 
-    // componentWillUnmount fires an instant before unmounting
-
-    //lifecycle method that is going to be triggered just once after initial loading
-    componentDidMount = async () => {
-        try {
-            let response = await fetch("https://striveschool.herokuapp.com/api/reservation")
-            let reservations = await response.json()
-            this.setState({ reservations: reservations, loading: false })
-        } catch (e) {
-            console.log("error happened, that's life", e)
-            this.setState({ loading: false })
-        }
-    }
-
-
-    render() {
-
-        console.log('IN THE RENDER METHOD')
-
-        return (
-            <Container>
-                <div className="mb-5">
-                    <h2>RESERVATIONS</h2>
-                    {
-                        this.state.loading && (
-                            <div className="font-bold d-flex justify-content-center">
-                                <span>Feching reservations</span>
-                                <Spinner animation="border" variant="success" />
-                            </div>
-                        )
-                    }
-                    {this.state.reservations.map((reservation, index) => (
-                        <ListGroup key={index}>
-                            <ListGroup.Item>
-                                Name: {reservation.name}, for {reservation.numberOfPersons}
-                            at {reservation.dateTime}
-                            </ListGroup.Item>
-                        </ListGroup>
-                    ))}
-                    <ReservationForm />
+  render() {
+    console.log("IN THE RENDER METHOD");
+    return (
+      <Container>
+        <div className="mb-5">
+          {this.state.errMessage ? (
+            <Alert variant="danger">
+              We encountered a problem with fetching reservation, please try
+              again later
+              {this.state.errMessage}
+            </Alert>
+          ) : (
+            <>
+              <h2>RESERVATIONS</h2>
+              {this.state.loading && (
+                <div className="font-bold d-flex justify-content-center">
+                  <span>Feching reservations</span>
+                  <Spinner animation="border" variant="success" />
                 </div>
-            </Container>
-        )
-    }
+              )}
+              {this.state.reservations.map((reservation, index) => (
+                <ListGroup key={index}>
+                  <ListGroup.Item>
+                    Name: {reservation.name}, for {reservation.numberOfPersons}
+                    at {reservation.dateTime}
+                  </ListGroup.Item>
+                </ListGroup>
+              ))}
+              <ReservationForm />
+            </>
+          )}
+        </div>
+      </Container>
+    );
+  }
 }
 
-export default Reservations
+export default Reservations;
